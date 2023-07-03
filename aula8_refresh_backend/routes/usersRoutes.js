7ae5b2a7063266284650686305ac22b47b2aba0b
Alberto Da Silva Cordeiro <albertoc@br.ibm.com>
2023-07-03 18:58:06 -0300

    FIX - limpeza de codigo

--- aula8_refresh_backend/routes/usersRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const usersController = require('../controllers/usersController')
-
-router.get('/', usersController.getUsers)
-router.post('/',usersController.addUser)
-
-module.exports= router
